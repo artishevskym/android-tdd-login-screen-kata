de6b7cba45dd385133b00c8a8e97d95f05593618
artishevskym <56644443+artishevskym@users.noreply.github.com>
2022-05-01 18:25:30 +0100

    GREEN: test password input is displayed

--- app/src/main/java/com/artishevsky/loginscreenkata/presentation/LoginScreen.kt
+++ app/src/main/java/com/artishevsky/loginscreenkata/presentation/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.artishevsky.loginscreenkata.presentation
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.artishevsky.loginscreenkata.R
 import com.artishevsky.loginscreenkata.ui.theme.LoginScreenKataTheme
 
@@ -29,6 +32,14 @@ fun LoginScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .testTag("emailTextField"),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = stringResource(R.string.login_password_input_text),
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("passwordTextField")
             )
         }
     }
